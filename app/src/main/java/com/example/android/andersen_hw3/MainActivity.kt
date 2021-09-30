79d635e5e03629928d1f404c639f6d971e4f54b3
VadimSurits <sourits-moscow@mail.ru>
2021-09-30 18:09:27 +1000

    1 task completed

--- app/src/main/java/com/example/android/andersen_hw3/MainActivity.kt
+++ app/src/main/java/com/example/android/andersen_hw3/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.android.andersen_hw3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.andersen_hw3.task_one.TaskOneActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchTaskOneActivity(view: View) {
+        val intentTaskOne = Intent(this, TaskOneActivity::class.java)
+        startActivity(intentTaskOne)
+    }
+
+    fun launchTaskTwoActivity(view: View) {
+
     }
 }
---
+++ app/src/main/java/com/example/android/andersen_hw3/task_one/TaskOneActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.andersen_hw3.task_one
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.andersen_hw3.R
+
+class TaskOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_one)
+    }
+}
