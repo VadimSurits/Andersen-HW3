abecb1a0730122f08b787120a558ebdf35daee53
VadimSurits <sourits-moscow@mail.ru>
2021-10-02 02:53:43 +1000

    Task "Additional complication" completed

--- app/src/main/java/com/example/android/andersen_hw3/task_two/TaskTwoActivity.kt
+++ app/src/main/java/com/example/android/andersen_hw3/task_two/TaskTwoActivity.kt
@@ -1,6 +1,9 @@
 package com.example.android.andersen_hw3.task_two
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
@@ -31,14 +34,15 @@ class TaskTwoActivity : AppCompatActivity() {
         mEditText.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 val url = v.text.toString()
-                tryLoadPicture(url)
+//                tryLoadPictureWithGlide(url)
+                tryLoadPictureWithAsyncTask(url)
                 return@OnEditorActionListener true
             }
             false
         })
     }
 
-    private fun tryLoadPicture(url: String?) {
+    private fun tryLoadPictureWithGlide(url: String?) {
         Glide.with(this)
             .load(url)
             .listener(object : RequestListener<Drawable> {
@@ -46,11 +50,7 @@ class TaskTwoActivity : AppCompatActivity() {
                     e: GlideException?, model: Any?,
                     target: Target<Drawable>?, isFirstResource: Boolean
                 ): Boolean {
-                    Toast.makeText(
-                        this@TaskTwoActivity,
-                        getString(R.string.load_failed_message),
-                        Toast.LENGTH_LONG
-                    ).show()
+                    showToast()
                     return true
                 }
 
@@ -58,10 +58,47 @@ class TaskTwoActivity : AppCompatActivity() {
                     resource: Drawable?, model: Any?, target: Target<Drawable>?,
                     dataSource: DataSource?, isFirstResource: Boolean
                 ): Boolean {
-                    Log.d(logTag, "OnResourceReady")
+                    Log.d(logTag, getString(R.string.on_resource_ready_log_message))
                     return false
                 }
             })
             .into(mImageView)
+    }
+
+    private fun tryLoadPictureWithAsyncTask(url: String?) {
+        val myTask = AdditionalTask(mImageView)
+        myTask.execute(url)
+    }
+
+    private inner class AdditionalTask(var imageView: AppCompatImageView) :
+        AsyncTask<String, Void, Bitmap?>() {
+
+        override fun doInBackground(vararg urls: String?): Bitmap? {
+            val imageUrl = urls[0]
+            var image: Bitmap? = null;
+            try {
+                val incoming = java.net.URL(imageUrl).openStream()
+                image = BitmapFactory.decodeStream(incoming)
+            } catch (e: Exception) {
+                Log.e(logTag, getString(R.string.asynс_task_download_error))
+                e.printStackTrace()
+                runOnUiThread {
+                    showToast()
+                }
+            }
+            return image
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            imageView.setImageBitmap(result)
+        }
+    }
+
+    private fun showToast() {
+        Toast.makeText(
+            this@TaskTwoActivity,
+            getString(R.string.load_failed_message),
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
